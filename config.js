a22b569e34b55d28b3c1d474a04f593c4cd6e1aa
Felipe Diep <felipe@boalt.com>
2014-08-07 17:28:02 -0400

    config file holding pub nub keys

---
+++ config.js
@@ -0,0 +1,8 @@
+//Config file
+
+// PubNub Global Definitions
+global.PUBNUB = {
+  publish_key   : "pub-c-XXXXXXXXXXX", //make sure you get your publish key
+  subscribe_key : "sub-c-XXXXXXXXXXX", //make sure you get your subscribe key
+  channel_01: "my_channel"
+};
